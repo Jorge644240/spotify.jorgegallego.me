965025c10c1b146e1cf7b7d638f8883075230c8a
Jorge Gallego <gallegojorge908@gmail.com>
2021-05-11 13:38:09 -0500

    Updated port value

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const spotifyWebApi = require("spotify-web-api-node");
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(`${__dirname}/static`));
 app.set("view engine", "pug");
