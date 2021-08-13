b20ecefffcd8a82f2a37bdc33e7f5bc02f053c63
Jorge Gallego <gallegojorge908@gmail.com>
2021-08-13 10:41:02 -0500

    Added responsiveness

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get("/", (req, res) => {
 
 app.get("/results", (req, res) => {
     let spotifyApi = new spotifyWebApi({
-        clientId: /* Client ID */'',
-        clientSecret: /* Client Secret */'',
+        clientId: 'a553526c85c44cdc8014afc63da0891a',
+        clientSecret: '2805f74937134f028fbfad4dc9952c56',
         redirectUri: 'https://example.com/callback',
     });
     spotifyApi.clientCredentialsGrant()
