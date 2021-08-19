0be5316313e479c1bd0ce68bb26116b1e9fd4fee
Jorge Gallego <gallegojorge908@gmail.com>
2021-08-19 14:17:11 -0500

    Updated on scroll animation options

--- static/js/songs.js
+++ static/js/songs.js
@@ -2,4 +2,7 @@ for (let result of document.querySelectorAll("#results div"))
 {
     result.setAttribute('data-aos', 'fade-up');
 }
-AOS.init({duration: 1000});
+AOS.init({
+    duration: 1000,
+    once: true
+});
